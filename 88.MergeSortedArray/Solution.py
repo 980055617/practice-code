38e8efae0de167f3604570fa3eb23dce5be7902e
980055617 <y980055617@gmail.com>
2023-08-08 10:31:44 +0900

    update

--- 88.MergeSortedArray/Solution.py
+++ 88.MergeSortedArray/Solution.py
@@ -1,32 +1,22 @@
 class Solution:
-    def merge(self, nums1: list[int], m: int, nums2: list[int], n: int) -> None:
-        """
-        Do not return anything, modify nums1 in-place instead.
-        """
+    def removeElement(self, nums: list[int], val: int) -> int:
+        cnt =0
         j = 0
-        # check up nums2
-        for i in nums2:
-
-            # nums1 smaller than nums2
-            while nums1[j] < i and j<m:
-                j += 1
-
-            # nums1 out of range
-            if j >= m:
-                nums1[j] = i
-
-            # insert nums2 between nums1
-            else:
-                nums1.insert(j,i)
-                nums1.pop()
+        for i in range(len(nums)):
+            # replace num to 0 when same as val
+            if nums[j] == val:
+                nums.pop(j)
+                nums.append(0)
+                j -= 1
+                cnt+=1
             j +=1
-            m +=1
+        k = len(nums) -cnt
+        return k
+
 
 if __name__ == '__main__':
-    nums1 = [1,2,3,0,0,0]
-    m = 3
-    nums2 = [2,5,6]
-    n = 3
+    nums = [3,2,2,3]
+    val = 3
     solution = Solution()
-    solution.merge(nums1,m,nums2,n)
-    print(nums1)
+    solution.removeElement(nums,val)
+    print(nums)

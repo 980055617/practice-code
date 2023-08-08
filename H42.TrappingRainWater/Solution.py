14e5e3ca7a64e4fdc941a8b5f536a472a7d32a7f
980055617 <y980055617@gmail.com>
2023-08-08 16:21:34 +0900

    Update

---
+++ H42.TrappingRainWater/Solution.py
@@ -0,0 +1,9 @@
+class Solution:
+    def trap(self, height: list[int]) -> int:
+        pass
+
+
+if __name__ == '__main__':
+    height = [0,1,0,2,1,0,1,3,2,1,2,1]
+    solution = Solution()
+    print(solution.trap(height))

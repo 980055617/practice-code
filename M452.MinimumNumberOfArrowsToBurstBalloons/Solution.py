6b3938cf4438e5c98b39ed00d9623038ee53f8c8
980055617 <y980055617@gmail.com>
2023-08-16 13:22:52 +0900

    Update

--- M452.MinimumNumberOfArrowsToBurstBalloons/Solution.py
+++ M452.MinimumNumberOfArrowsToBurstBalloons/Solution.py
@@ -1,44 +1,19 @@
 class Solution:
-    def insert(self, intervals: list[list[int]], newInterval: list[int]) -> list[list[int]]:
-
-        startIndex, endIndex, length, ans = -1, -1, len(intervals), []
-
-        if length == 0:
-            ans.append(newInterval)
-            return ans
-
-        for i in range(length):
-            if newInterval[0] <= intervals[i][1] and startIndex == -1:
-                startIndex = i
-            if newInterval[1] >= intervals[i][0]:
-                endIndex = i
+    def findMinArrowShots(self, points: list[list[int]]) -> int:
+        points = sorted(points)
+        ans = 1
+        range = points[0][1]
+        for start, end in points[1:]:
+            if start <= range:
+                if end < range:
+                    range = end
             else:
-                break
-        else:
-            endIndex = length - 1
-
-        if startIndex == -1:
-            intervals.append(newInterval)
-            return intervals
-
-        if endIndex == -1:
-            intervals.insert(0,newInterval)
-            return intervals
-
-        if endIndex < startIndex:
-            intervals.insert(startIndex,newInterval)
-            return intervals
-
-        for i in range(length):
-            if i < startIndex or i > endIndex:
-                ans.append(intervals[i])
-            elif i == startIndex:
-                ans.append([min(intervals[startIndex][0],newInterval[0]),max(intervals[endIndex][1],newInterval[1])])
+                ans += 1
+                range = end
 
         return ans
 
 if __name__ == '__main__':
-    intervals = [[3,5],[12,15]]
-    newInterval = [6,6]
+    points = [[10,16],[2,8],[1,6],[7,12]]
     solution = Solution()
-    print(solution.insert(intervals,newInterval))
+    print(solution.findMinArrowShots(points))

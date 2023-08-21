f0cc9b217d148dc1a8035f60ba130a57a9d119f9
Tatsuru Kawanishi <y980055617@gmail.com>
2023-08-21 09:56:10 +0900

    Update

--- M150.EvaluateReversePolishNotation/Solution.py
+++ M150.EvaluateReversePolishNotation/Solution.py
@@ -1,22 +1,12 @@
 class Solution:
-    def rotate(self, nums: list[int], k: int) -> None:
-        """
-        Do not return anything, modify nums in-place instead.
-        """
-        if k == 0:
-            pass
-        else:
-            length =len(nums)
-            k = k % length
-            list = nums[-k:]
-            list1 = list + nums[:length-k]
-            nums.clear()
-            nums.extend(list1)
-            #print(nums)
+    def evalRPN(self, tokens: list[str]) -> int:
+        tmp = 0
+        unUsed = []
+        for i in tokens:
+            if i.isdigit():
+        return None
 
 if __name__ == '__main__':
-    nums = [1,2,3,4,5,6,7]
-    k = 3
+    tokens = ["2","1","+","3","*"]
     solution = Solution()
-    solution.rotate(nums,k)
-    print(nums)
+    print(solution.evalRPN(tokens))

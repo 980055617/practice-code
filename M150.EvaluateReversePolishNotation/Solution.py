33ab2b9dea3e62d4ae651faf6fe68846692d8184
Tatsuru Kawanishi <y980055617@gmail.com>
2023-08-28 09:56:51 +0900

    Update

--- M150.EvaluateReversePolishNotation/Solution.py
+++ M150.EvaluateReversePolishNotation/Solution.py
@@ -3,13 +3,27 @@ class Solution:
         tmp = 0
         unUsed = []
         for i in tokens:
-            if i.isdigit():
-                unUsed.append(i)
+            if i == '+':
+                a = unUsed.pop()
+                b = unUsed.pop()
+                unUsed.append(b+a)
+            elif i == '-':
+                a = unUsed.pop()
+                b = unUsed.pop()
+                unUsed.append(b-a)
+            elif i == '*':
+                a = unUsed.pop()
+                b = unUsed.pop()
+                unUsed.append(b*a)
+            elif i == '/':
+                a = unUsed.pop()
+                b = unUsed.pop()
+                unUsed.append(b//a)
             else:
-                
-        return None
+                unUsed.append(float(i))
+        return unUsed.pop()
 
 if __name__ == '__main__':
-    tokens = ["2","1","+","3","*"]
+    tokens = ["10","6","9","3","+","-11","*","/","*","17","+","5","+"]
     solution = Solution()
     print(solution.evalRPN(tokens))
